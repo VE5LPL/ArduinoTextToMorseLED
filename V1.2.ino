047a65ea9baa335c61d2b7740639a73e5d59b85a
VE5LPL <VE5LPL@Ktecknix.com>
2017-03-31 14:58:38 -0600

    DO NOT RUN, NOT COMPLETE
    If you would like to help, feel free, I'm trying to get where you put you text into one place easy, now how do I run a function from the value of a variable?

---
+++ V1.2.ino
@@ -0,0 +1,207 @@
+//NO F FUNCTION
+//FIX LOOP FUNCTION
+int chars = 6;
+int charnum = chars + 1;
+char string[] = "KEATON";
+int led = 13;
+
+void dit(){
+digitalWrite(led, HIGH); 
+delay(250);             
+digitalWrite(led, LOW);   
+delay(250);
+}
+
+void daw(){
+digitalWrite(led, HIGH);  
+delay(750);              
+digitalWrite(led, LOW);    
+delay(250);
+}
+
+void A(){
+  dit();
+  daw();
+  delay(1750);
+}
+
+void B(){
+  daw();
+  dit();
+  dit();
+  dit();
+  delay(1750);
+}
+
+void C(){
+  daw();
+  dit();
+  daw();
+  dit();
+  delay(1750);
+}
+
+void D(){
+ daw();
+ dit();
+ dit();
+ delay(1750);
+}
+
+void E(){
+  dit();
+  delay(1750);
+}
+
+
+
+void G(){
+  daw();
+  daw();
+  dit();
+  delay(1750);
+}
+
+void H(){
+  dit();
+  dit();
+  dit();
+  dit();
+  delay(1750);
+}
+
+void I(){
+  dit();
+  dit();
+  delay(1750);
+}
+
+void J(){
+  dit();
+  daw();
+  daw();
+  daw();
+  delay(1750);
+}
+
+void K(){
+  daw();
+  dit();
+  daw();
+  delay(1750);
+}
+void L(){
+  dit();
+  daw();
+  dit();
+  dit();
+  delay(1750);
+}
+
+void M(){
+ daw();
+ daw();
+ delay(1750);
+}
+
+void N(){
+ daw();
+ dit();
+ delay(1750);
+}
+
+void O(){
+ daw();
+ daw();
+ daw();
+ delay(1750);
+}
+
+void P(){
+ dit();
+ daw();
+ daw();
+ dit();
+ delay(1750);
+}
+
+void Q(){
+ daw();
+ daw();
+ dit();
+ daw();
+ delay(1750);
+}
+
+void R(){
+ dit();
+ daw();
+ dit();
+ delay(1750);
+}
+
+void S(){
+ dit();
+ dit();
+ dit();
+ delay(1750);
+}
+
+void T(){
+ daw();
+ delay(1750);
+}
+
+void U(){
+ dit();
+ dit();
+ daw();
+ delay(1750);
+}
+
+void V(){
+ dit();
+ dit();
+ dit();
+ daw(); 
+ delay(1750);
+}
+
+void W(){
+ dit();
+ daw();
+ daw();
+ delay(1750);
+}
+
+void X(){
+ daw();
+ dit();
+ dit();
+ daw();
+ delay(1750);
+}
+
+void Y(){
+ daw();
+ dit();
+ daw(); 
+ daw();
+ delay(1750);
+}
+
+void Z(){
+ daw();
+ daw();
+ dit();
+ dit();
+ delay(1750);
+}
+
+void loop(){
+  while (true) {
+    for (char i = 0; i < charnum; i++){
+     
+    }  
+  }
+}
